af03dc747a3b6fd62254520042480b30ee3a94dc
Marcos Vinicius <marcos@bw2.com.br>
2019-11-07 20:05:34 -0300

    adicionado alteração tela login e home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,6 +3,7 @@ import CardProdutoHome from './CardProdutoHome';
 import { connect } from 'react-redux';
 import { loadProdutos, setLoadProduto } from '../reduxStore/actions';
 import Utils from '../Utils';
+import Slider from "react-slick";
 
 
 class Home extends React.Component {
@@ -13,6 +14,13 @@ class Home extends React.Component {
     }
 
     render() {
+        const settings = {
+            dots: false,
+            infinite: false,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 4,
+        };
         return (
             <>
                 {
@@ -30,7 +38,31 @@ class Home extends React.Component {
                             );
                         })
                 }
+                
             </>
+             <div>
+            <h2> Single Item</h2>
+            <Slider {...settings}>
+              <div>
+                <h3>1</h3>
+              </div>
+              <div>
+                <h3>2</h3>
+              </div>
+              <div>
+                <h3>3</h3>
+              </div>
+              <div>
+                <h3>4</h3>
+              </div>
+              <div>
+                <h3>5</h3>
+              </div>
+              <div>
+                <h3>6</h3>
+              </div>
+            </Slider>
+          </div>
         )
     }
 }
