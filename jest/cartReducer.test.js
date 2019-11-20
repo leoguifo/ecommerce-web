28f191663bfdc2fa803ab13164e8067f058d0f6f
Leonardo Guilherme Fodi <32180418+leoguifo@users.noreply.github.com>
2019-11-20 20:37:09 -0300

    Test example
    Test example for nathan

--- jest/cartReducer.test.js
+++ jest/cartReducer.test.js
@@ -1,5 +1,5 @@
 import { cartReducer } from '../src/reduxStore/reducers/cartReducer';
-import { UPDATE_CART } from '../src/reduxStore/actions/actionTypes';
+import { UPDATE_CART, ADD_ITEM_TO_CART } from '../src/reduxStore/actions/actionTypes';
 
 describe('Cart reducer test', () => {
 
@@ -21,7 +21,7 @@ describe('Cart reducer test', () => {
             total: 10,
             itensCounter: 1
         })
-        .toEqual(initialState);
+            .toEqual(initialState);
 
     });
 
@@ -29,11 +29,11 @@ describe('Cart reducer test', () => {
 
         let initialState = {};
 
-        initialState = cartReducer(undefined, { 
+        initialState = cartReducer(undefined, {
             type: UPDATE_CART,
             itensCounter: 2,
             total: 100
-         });
+        });
 
         expect({
             codigo: "132",
@@ -47,8 +47,45 @@ describe('Cart reducer test', () => {
             total: 100,
             itensCounter: 2
         })
-        .toEqual(initialState);
+            .toEqual(initialState);
 
+    });
+
+    test('ADD_ITEM_TO_CART test', () => {
+        let initialState = {};
+
+        initialState = cartReducer(undefined, {
+            type: ADD_ITEM_TO_CART,
+            itens: {
+                sku: "666",
+                nome: "Teste",
+                descricao: "Teste produto",
+                preco: 100,
+                imagem: "https://materializecss.com/images/sample-1.jpg"
+            }
+        });
+
+        console.warn(initialState);
+
+        expect({
+            codigo: "132",
+            itens: [{
+                sku: "321",
+                nome: "Teste",
+                descricao: "Produto é assim, assim e assado",
+                preco: 10,
+                imagem: "https://materializecss.com/images/sample-1.jpg"
+            }, {
+                sku: "666",
+                nome: "Teste",
+                descricao: "Teste produto",
+                preco: 100,
+                imagem: "https://materializecss.com/images/sample-1.jpg"
+            }],
+            total: 100,
+            itensCounter: 2
+        }
+        ).toEqual(initialState.itens);
     });
 
 });
