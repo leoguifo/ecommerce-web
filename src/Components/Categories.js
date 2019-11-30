48dbe540aeb76fb738b3a0a1faffd5e888346946
Marcos <vi.niso@hotmail.com>
2019-11-30 20:56:40 -0300

    ended structure of categorie page

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -8,7 +8,7 @@ export default class Categories extends React.Component {
         return (
             <div className="col s12 ">
                 <div className="col-sm-12 col-lg-6 offset-l3">
-                    <ul class="w3-border w3-ul center" >
+                    <ul class="w3-border w3-ul " style={styles.ul} >
                         <li type="button" style={styles.cat}>Ação</li>
                         <li type="button" style={styles.cat}>Aventura</li>
                         <li type="button" style={styles.cat}>RPG</li>
@@ -28,6 +28,11 @@ const styles = {
     cat:{
         margin: '5px',
         borderStyle: 'ridge',
-
+        width: '75%',
+        backgroundColor: 'white',
+        height: '30px',
+    },
+    ul:{
+        textAlign: '-webkit-center',
     },
 }
