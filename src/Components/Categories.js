b8d660759c6582364f81bcb2b701ebe1ab7b2fd5
Marcos <vi.niso@hotmail.com>
2019-12-02 22:12:46 -0300

    added changes to categories list

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import {Link} from "react-router-dom";
+import Home from './Home';
 
 
 export default class Categories extends React.Component {
@@ -9,16 +10,19 @@ export default class Categories extends React.Component {
             <div className="col s12 ">
                 <div className="col-sm-12 col-lg-6 offset-l3">
                     <ul class="w3-border w3-ul " style={styles.ul} >
-                        <li type="button" style={styles.cat}>Ação</li>
-                        <li type="button" style={styles.cat}>Aventura</li>
-                        <li type="button" style={styles.cat}>RPG</li>
-                        <li type="button" style={styles.cat}>Ação</li>
-                        <li type="button" style={styles.cat}>Aventura</li>
-                        <li type="button" style={styles.cat}>RPG</li>
-                        <li type="button" style={styles.cat}>Ação</li>
-                        <li type="button" style={styles.cat}>Aventura</li>
-                        <li type="button" style={styles.cat}>RPG</li>
+                        <li className="waves-effect waves-light btn-small" type="button" style={styles.cat}>Ação</li>
+                        <li className="waves-effect waves-light btn-small" type="button" style={styles.cat}>Aventura</li>
+                        <li className="waves-effect waves-light btn-small" type="button" style={styles.cat}>RPG</li>
+                        <li className="waves-effect waves-light btn-small" type="button" style={styles.cat}>Ação</li>
+                        <li className="waves-effect waves-light btn-small" type="button" style={styles.cat}>Aventura</li>
+                        <li className="waves-effect waves-light btn-small" type="button" style={styles.cat}>RPG</li>
+                        <li className="waves-effect waves-light btn-small" type="button" style={styles.cat}>Ação</li>
+                        <li className="waves-effect waves-light btn-small" type="button" style={styles.cat}>Aventura</li>
+                        <li className="waves-effect waves-light btn-small" type="button" style={styles.cat}>RPG</li>
                     </ul>
+                    <div>
+                    <Link className="waves-effect waves-light btn-small " style={styles.cat} to="/Home">Voltar</Link>
+                    </div>
                 </div>
             </div>
         )
@@ -26,11 +30,11 @@ export default class Categories extends React.Component {
 }
 const styles = {
     cat:{
-        margin: '5px',
+        margin: '2px',
         borderStyle: 'ridge',
-        width: '75%',
-        backgroundColor: 'white',
-        height: '30px',
+        width: '95%',
+        backgroundColor: 'black',
+        height: '45px',
     },
     ul:{
         textAlign: '-webkit-center',
