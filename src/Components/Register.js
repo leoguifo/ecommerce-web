8afa23d0c1674e566cf22709753ef11bc33c1d6b
Lucien Risso Correia <lucien@multiplier.com.br>
2019-12-05 23:01:44 -0300

    cadastro

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -2,9 +2,56 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Utils from '../Utils';
 import { registerLogin } from '../reduxStore/actions'
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
+import firebase from '../Firebase/index'
+
 
 class Register extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            nome: '',
+            email: '',
+            senha: '',
+            senhaConfirmar: ''
+        };
+
+        this.register = this.register.bind(this);
+    }
+
+    register = () => {
+
+        if (this.state.senha !== this.state.senhaConfirmar) {
+            window.M.toast({ html: 'Senhas não são iguais!' });
+            return;
+        }
+
+        let that = this;
+
+        firebase.auth()
+            .createUserWithEmailAndPassword(this.state.email, this.state.senha)
+            .then((usuario) => {
+                usuario.user.updateProfile({
+                    displayName: that.state.nome
+                });
+                console.log(usuario);
+            }).catch((err) => {
+                if (err.code === "auth/invalid-email") {
+                    window.M.toast({ html: 'Email inválido!' });
+                }
+
+                if (err.code === "auth/wrong-password") {
+                    window.M.toast({ html: 'Email já em uso!' });
+                }
+
+                if (err.code === "auth/weak-password") {
+                    window.M.toast({ html: 'Senha fraca!' });
+                }
+                console.error(err);
+            });
+    }
 
     render() {
         return (
@@ -18,20 +65,20 @@ class Register extends React.Component {
                             <form>
                                 <div style={styles.prin} >
                                     <label>
-                                        <input placeholder="Usuario" type="text" name="name" />
+                                        <input placeholder="Usuario" type="text" value={this.state.nome} onChange={(e) => this.setState({ nome: e.target.value })} />
                                     </label>
                                     <label>
-                                        <input placeholder="Senha" type="password" name="password" />
+                                        <input placeholder="Senha" type="password" value={this.state.senha} onChange={(e) => this.setState({ senha: e.target.value })}/>
                                     </label>
                                     <label>
-                                        <input placeholder="Confirmar Senha" type="password" name="password" />
+                                        <input placeholder="Confirmar Senha" type="password" value={this.state.senhaConfirmar} onChange={(e) => this.setState({ senhaConfirmar: e.target.value })} />
                                     </label>
                                     <label>
-                                        <input placeholder="E-mail" type="text" name="name" />
+                                        <input placeholder="E-mail" type="text" value={this.state.email} onChange={(e) => this.setState({ email: e.target.value })} />
                                     </label>
                                 </div>
                                 <div style={styles.ula}>
-                                    <Link className="waves-effect waves-light btn-small" value="Enviar" onClick={ () => this.props.registerLogin({ email: window.$("emailR").val(), password: window.$("passR").val() }) } style={styles.cat} to="/Login">Enviar</Link>
+                                    <button type="button" className="waves-effect waves-light btn-small" onClick={this.register} style={styles.cat} to="/Login">Enviar</button>
                                  </div>
                             </form>
                             <p className="red-text"><b>{this.props.loginError}</b></p>
